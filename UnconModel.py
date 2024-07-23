2ff67b0935f6548193ca48e21b4048da7b2f5bf9
T'Vek Saterk <176432906+DivisionMuEpsilonFleet@users.noreply.github.com>
2024-07-23 18:21:31 -0400

    Update UnconModel.py

--- UnconModel.py
+++ UnconModel.py
@@ -1,4 +1,11 @@
+import matplotlib.pyplot as plt
 test_time = 0
+# creating an empty canvas
+fig = plt.figure()
+ 
+# defining the axes with the projection
+# as 3D so as to plot 3D graphs
+ax = plt.axes(projection="3d")
 
 class UnconProc:
     def __init__(self, basecd, globalcd, runtime, name):
@@ -85,15 +92,30 @@ class Console:
         self.currentCD = self.currentCD - self.BaseCD*0.25
 
 def main(UnCons,Consoles):
+    time_stamps_for_procs                        = []
+    magnitude_of_uncon_procs                     = []
+    magnitude_of_console_activations             = []
+
     for t in range (0,test_time):
         UnConIterationCounter = 0
+
         for UnCon in UnCons:
             for console in Consoles:
                 if(UnConIterationCounter == 0):
                     console.decrement_console_time()
-                    console_name, triggered = console.try_trigger_console(t)
+                    console_name, console_was_triggered = console.try_trigger_console(t)
+
+                    time_stamps_for_procs.append(t)
+                    magnitude_of_uncon_procs.append(0)
+                    magnitude_of_console_activations.append(0)   
+                                  
+                    if(console_was_triggered == True):
+                        magnitude_of_console_activations[len(magnitude_of_console_activations)-1] = magnitude_of_console_activations[len(magnitude_of_console_activations)-1] + 1
                     
-                    if(console_name == "Subspace Fracture Tunneling Field" and triggered == True): #if SFTF was just activated
+                    
+                        
+
+                    if(console_name == "Subspace Fracture Tunneling Field" and console_was_triggered == True): #if SFTF was just activated
                                     for consoles_to_be_reduced_by_SFTF in Consoles:
                                         if (consoles_to_be_reduced_by_SFTF.get_Name() != "Subspace Fracture Tunneling Field"): #Ensure SFTF isn't reducing itself
                                             consoles_to_be_reduced_by_SFTF.apply_SFTF_Reduction()
@@ -102,8 +124,11 @@ def main(UnCons,Consoles):
                 if t % UnCon.get_GCD() == 0: #Then t / GCD has no remainder, ie we get an uncon proc.
                     #print(UnCon.get_Name() + " Activated at " + str(t))
                     console.apply_uncon()
+
                 console.elapse_time()
             UnConIterationCounter = UnConIterationCounter+1
+
+    return time_stamps_for_procs, magnitude_of_uncon_procs, magnitude_of_console_activations
 
   
 test_time                                = 250
@@ -128,9 +153,25 @@ Adaptive_Emergency_Systems               = Console(120,0,0,      "Adaptive Emerg
 Dynamic_Power_Redistribution_Module      = Console(120,0,20,     "Dynamic Power Redistribution Module")
 Subspace_Fracture_Tunneling_Field        = Console(30,0,0,       "Subspace Fracture Tunneling Field")
 
-
-
 UnCons_List = [CleanGetaway, TractorBeam, GravityWell, EjectWarpPlasma, PhotonicShockwave]
 Console_List = [Subspace_Fracture_Tunneling_Field, Adaptive_Emergency_Systems]
-main(UnCons_List,Console_List)
+X, Y, Z = main(UnCons_List,Console_List)
 
+ax.set_xlabel("Time")
+ax.set_ylabel("Number of Uncon Procs on Tick")
+ax.set_zlabel("Number of Consoles Activated")
+
+# plotting a scatter plot with X-coordinate,
+# Y-coordinate and Z-coordinate respectively
+# and defining the points color as cividis
+# and defining c as z which basically is a 
+# definition of 2D array in which rows are RGB
+#or RGBA
+
+
+
+#Generates full data set
+ax.scatter3D(X, Y, Z)
+
+# Showing the above plot
+plt.show()
