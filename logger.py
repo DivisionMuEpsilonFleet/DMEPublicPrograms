675122d08b75235b223dd00e9ae664902801776f
T'Vek Saterk <176432906+DivisionMuEpsilonFleet@users.noreply.github.com>
2024-11-05 10:28:36 -0500

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,8 +1,11 @@
 import pynput
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Listener  as KeyboardListener
+from pynput.mouse    import Listener  as MouseListener
+from pynput.keyboard import Key
 from datetime import datetime
 import time
 import pyuac
+import string
   
 keys = []
 start_time = time.time()
@@ -50,19 +53,22 @@ def check_special_keys(k):
     elif(k == "`"): #invalid in the general case but its the same key in sto keybinds so meh
             return "` or ~"
     
+    elif( k.isupper() ): #Just here to convert entries like "T" -> "Shift + t" for readability
+            return ("Shift + " + k.lower())
+    
     elif(k == "|"): #very specific key in my STO Keybinds file so I can do a video sychronization mark 
             global start_time  #ugly but it works and I don't care enough
             start_time = time.time()
-            with open('log.txt', 'w') as f:
+            with open('Keyboard_Inputs_Log.log', 'w') as f:
                   f.write("Logging Session On: " + str(datetime.now()) +  " Local Time Zone" + "\nVideo Synchronization Marker")
             return "| Video Synchronization Marker"
     
     else:
          return k
           
-def write_file(key):
+def write_file(key): #key may not always be a 'Key' Object, it may also be a string such as in the case of the mosue listener. 
     set_log_entry_event_length_limiter = 35 #used to format the log so that we can easily read it. This is *fixed* length all log entries will be. 
-    with open('log.txt', 'a') as f: #original code had it re-write the entire file each time with a list, which seemed very in-efficient and like it would cause memory issues. I am not sure though, how this append works. If it accepts the entire .txt file into memory, the original implemention might have been superior. 
+    with open('Keyboard_Inputs_Log.log', 'a') as f: #original code had it re-write the entire file each time with a list, which seemed very in-efficient and like it would cause memory issues. I am not sure though, how this append works. If it accepts the entire .txt file into memory, the original implemention might have been superior. 
        # removing ''
        k = str(key).replace("'", "")
        k = check_special_keys(k)
@@ -84,12 +90,23 @@ def on_release(key):
         # Stop listener
         return False
 
+def on_click(x, y, button, pressed):
+       if pressed:
+              mouse_singal_string = ""
+              if(str(button) == "Button.left" or str(button) == "Button.right"):#only really care about coordinates here when left or right pressing, so this only sends over 
+                    mouse_singal_string = str(button) + "@x=" + str(x) + "y=" + str(y) 
+              else:
+                    mouse_singal_string = button
+
+              write_file(mouse_singal_string)
+              #note, that write_file() takes a key object input, but it can still be accepted as a string, and the function automatically converts said inputs to string anyways
+
 #Running as Admin is nessecary in many games for whatever unknown reason. Cursory research seems to indicate issues with DirectX and Windows but honestly who knows :shrugs_non-chalantly:
 def main():
-       with Listener(on_press = on_press,
-              on_release = on_release) as listener:
-              listener.join()
-
+       with MouseListener(on_click=on_click) as listener:
+              with KeyboardListener(on_press = on_press, on_release = on_release) as listener:
+                     listener.join()
+    
 if __name__ == "__main__":
     if not pyuac.isUserAdmin():
         print("Re-launching as admin!")
