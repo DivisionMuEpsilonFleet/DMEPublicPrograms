039ba34b5f00bd760e2b04276ed127cb0add0769
T'Vek Saterk <176432906+DivisionMuEpsilonFleet@users.noreply.github.com>
2025-01-17 16:00:03 -0500

    Update T5PvPCalculations.py

--- T5PvPCalculations.py
+++ T5PvPCalculations.py
@@ -1,11 +1,13 @@
-class Player:
+lass Player:
+    Name = ""
     Wins = 0
     Losses = 0
     WeightedWins   = 0
     WeightedLosses = 0
 
 
-    def __init__(self, wins, losses,wwins,wlosses):
+    def __init__(self, name, wins, losses,wwins,wlosses):
+        self.Name           = name
         self.Wins           = wins
         self.Losses         = losses
         self.WeightedWins   = wwins
@@ -22,33 +24,37 @@ class Player:
         self.WeightedLosses    = self.Wins + (-1) * outcome * (enemy_comptence_multiplier / team_competence_multiplier)
 
     def get_winloss(self):
-        return (self.WeightedWins)/(self.WeightedLosses)
+        try: #to avoid division by zero error
+            return (self.WeightedWins)/(self.WeightedLosses)
+        except:
+            return 1
+        
+    def get_player_name(self):
+        print(self.Name)
+    
+
 
 class MatchRound:
     red_team    = []
     blue_team   = []
-    red_deaths  = []
-    blue_deaths = []
     OutcomeValue = 0 #1 for red team, -1 for blue team, 0 if a tie
 
-    def __init__(self, red_team, blue_team,red_deaths,blue_deaths, OutcomeValue):
+    def __init__(self, red_team, blue_team, OutcomeValue):
         self.red_team     = red_team
         self.blue_team    = blue_team
-        self.red_deaths   = red_deaths
-        self.blue_deaths  = blue_deaths
         self.OutcomeValue = OutcomeValue
 
     def calculate_mean_team_competence(self):
         red_team_size = len(self.red_team)
         red_team_competence = 0 #starting value
-        for red_player in range(0, red_team_size):
-            red_team_competence = red_team_competence + red_player.get_winloss()
+        for red_index in range(0, red_team_size):
+            red_team_competence = red_team_competence + self.red_team[red_index].get_winloss()
         red_team_competence = red_team_competence / red_team_size #this is the mean competence of the red team
 
         blue_team_size = len(self.blue_team)
         blue_team_competence = 0 #starting value
-        for blue_player in range(0,blue_team_size):
-            blue_team_competence = blue_team_competence + blue_player.get_winloss()
+        for blue_index in range(0,blue_team_size):
+            blue_team_competence = blue_team_competence + self.blue_team[blue_index].get_winloss()
         blue_team_competence = blue_team_competence / blue_team_size #this is the mean competence of the blue team
 
         return red_team_competence, blue_team_competence
@@ -58,9 +64,9 @@ class MatchRound:
         red_team_competence, blue_team_competence = self.calculate_mean_team_competence() #calculates mean competence of enemy team
         
         red_team_size = len(self.red_team)
-        for red_player in range(0, red_team_size):
-            red_player.update_player  ( 1*self.OutcomeValue  , blue_team_competence, red_team_competence)
+        for red_index in range(0, red_team_size):
+            self.red_team[red_index].update_player  ( 1*self.OutcomeValue  , blue_team_competence, red_team_competence)
        
         blue_team_size = len(self.blue_team)
-        for blue_player in range(0,blue_team_size):
-            blue_player.update_player ( -1*self.OutcomeValue , red_team_competence, blue_team_competence)
+        for blue_index in range(0,blue_team_size):
+            self.blue_team[blue_index].update_player ( -1*self.OutcomeValue , red_team_competence, blue_team_competence)
